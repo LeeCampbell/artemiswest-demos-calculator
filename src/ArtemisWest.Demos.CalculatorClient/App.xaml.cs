fc4211f4e646f8ab30194f8852a6acdbd90b38d4
Lee.Ryan.Campbell <Lee.Ryan.Campbell@90bce958-9d98-11de-8881-9de6a921b072>
2009-09-10 00:29:58 +0000

    Little clean up of unused files

--- src/ArtemisWest.Demos.CalculatorClient/App.xaml.cs
+++ src/ArtemisWest.Demos.CalculatorClient/App.xaml.cs
@@ -1,16 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Windows;
+﻿using System.IO;
 
 namespace ArtemisWest.Demos.CalculatorClient
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        public App()
+        {
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            //Would normally use an ILogger Interface as an adapter to a standard Logging library (EntLib, Log4Net, NLog...)
+            using (var writer = File.AppendText("Error.log"))
+            {
+                writer.WriteLine(e.Exception);
+            }
+            System.Windows.MessageBox.Show("An unhandled exception occured. The application is shutting down.");
+            e.Handled = true;
+            Shutdown(-1);
+        }
     }
 }
